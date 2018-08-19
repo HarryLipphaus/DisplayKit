d4542c5d2e250d0cf74b39bd9837c80f09ac8a7a
Harry Lipphaus <DerTrickreiche@gmail.com>
2018-08-19 12:08:03 +0200

    first commit

--- lcd.c
+++ lcd.c
@@ -30,15 +30,6 @@
  *
  *  lcd.h
  *
- *  Created by Michael Köhler on 22.12.16.
- *  Copyright 2016 Skie-Systems. All rights reserved.
- *
- *  lib for OLED-Display with ssd1306/sh1106-Controller
- *  first dev-version only for I2C-Connection
- *  at ATMega328P like Arduino Uno
- *
- *  at GRAPHICMODE lib needs SRAM for display
- *  DISPLAY-WIDTH * DISPLAY-HEIGHT + 2 bytes
  *
  *  14.08.2018 Harry L.
  *  added STM32/HAL Support
@@ -169,31 +160,6 @@ const uint8_t initSequence[] PROGMEM =
 
 // Chargepump
 		OLED_CHARGEPUMP, 0x14			// Set DC-DC enable
-
-/*			OLED_DISPLAYOFF,
-		   0x20, 0b00,         // Set Memory Addressing Mode
-		                                // 00=Horizontal Addressing Mode; 01=Vertical Addressing Mode;
-		                                // 10=Page Addressing Mode (RESET); 11=Invalid
-		    0xB0,                       // Set Page Start Address for Page Addressing Mode, 0-7
-		    0xC8,                       // Set COM Output Scan Direction
-		    0x00,                       // --set low column address
-		    0x10,                       // --set high column address
-		    0x40,                       // --set start line address
-		    0x81, 0x3F,         // Set contrast control register
-		    0xA1,                       // Set Segment Re-map. A0=address mapped; A1=address 127 mapped.
-		    0xA6,                       // Set display mode. A6=Normal; A7=Inverse
-		    0xA8, 0x3F,         // Set multiplex ratio(1 to 64)
-		    0xA4,                       // Output RAM to Display
-		                                // 0xA4=Output follows RAM content; 0xA5,Output ignores RAM content
-		    0xD3, 0x00,         // Set display offset. 00 = no offset
-		    0xD5,                       // --set display clock divide ratio/oscillator frequency
-		    0xF0,                       // --set divide ratio
-		    0xD9, 0x22,         // Set pre-charge period
-		    0xDA, 0x12,         // Set com pins hardware configuration
-		    0xDB,                       // --set vcomh
-		    0x20,                       // 0x20,0.77xVcc
-		    0x8D, 0x14,         // Set DC-DC enable
-/* */
 
 		};
 
